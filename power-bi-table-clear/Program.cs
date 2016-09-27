1a8c18312739ace8f7201bf595081daf0c11bd25
Jeff Hollan <jeffhollan@outlook.com>
2016-09-26 21:27:53 -0400

    Add project files.

---
+++ power-bi-table-clear/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_bi_table_clear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
